aba2a56de740ec55e90ce1a8e742388e18f8fab7
Rafael Cruz <cruz.ricky.tech@gmail.com>
2024-04-11 13:20:00 -0400

    adding map onto the app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import 'leaflet/dist/leaflet.css'; // Leaflet CSS
+import React, { useEffect } from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
 import logo from './logo.svg';
 import './App.css';
-import MyMap from './MyMap'; // Import the MyMap component
 
 function App() {
+  useEffect(() => {
+    // Creating map options
+    var mapOptions = {
+      center: [42.3128542, -71.0383129],
+      zoom: 10
+    };
+
+    // Creating a map object
+    var map = L.map('map', mapOptions);
+
+    // Creating a Layer object
+    var layer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+    // Adding layer to the map
+    map.addLayer(layer);
+
+    // Clean up the map when the component unmounts
+    return () => {
+      map.remove();
+    };
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -21,7 +43,7 @@ function App() {
           Learn React
         </a>
       </header>
-      <MyMap /> {/* Include the MyMap component to display the map */}
+      <div id="map" style={{ width: '100%', height: '500px' }}></div>
     </div>
   );
 }
