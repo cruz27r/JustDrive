a1b97877d20b6be37de99d154a9c9c74367f904f
BattleSkorpion <darian040904@gmail.com>
2024-04-16 13:46:26 -0400

    Update App.js

--- App.js
+++ App.js
@@ -19,29 +19,52 @@
 //   },
 // });
 
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Button} from 'react-native';
 import { LatLng, LeafletView } from 'react-native-leaflet-view';
 
 const App = () => {
+  const [mapCenter, setMapCenter] = useState({
+    lat: 42.328542,
+    lng: -71.0383129,
+  });
+
+  const moveToPosition = () => {
+    // mapRef.current.animateToRegion({
+    //   latitude: 42.3128542,
+    //   longitude: -71.0383129,
+    //   latitudeDelta: 0.0922,
+    //   longitudeDelta: 0.0421,
+    // }, 1000); // duration in milliseconds
+    setMapCenter({
+      lat: 42.3128542,
+      lng: -72.0383129,
+    });
+  };
+
   return (
-    <LeafletView
-      mapMarkers = {[
-        {
-          position: [42.3128542, -71.0383129],
-          icon: 'a',  
-          size: [32, 32],          
-        },
-      ]}
-      mapCenterPosition={{
-        lat: 42.3128542,
-        lng: -71.0383129,
-      }}
-    />
+    <View style={{ flex: 1}}>
+      <LeafletView
+        mapMarkers = {[
+          {
+            position: [42.3128542, -71.0383129],
+            icon: 'a',  
+            size: [32, 32],          
+          },
+        ]}
+        mapCenterPosition={mapCenter}
+        
+      />
+      <View style = {styles.buttonContainer}>
+        <Button title="Move Map" onPress={moveToPosition} />
+      </View>
+    </View>
   );
 }
 
 export default App;
+
+
 
 const styles = StyleSheet.create({
   page: {
@@ -53,7 +76,13 @@ const styles = StyleSheet.create({
     height: 300,
     width: 300,
   },
+  buttonContainer: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+  },
   map: {
     flex: 1
   }
 });
+
