b021e42a9f23fd91e2d3aaeb8590c5a494878f7b
Rafael Cruz <cruz.ricky.tech@gmail.com>
2024-05-01 22:30:34 -0400

    Directory and code fromatted for JS REACT project

---
+++ JustDrive-Web/src/App.js
@@ -0,0 +1,22 @@
+import './css/App.css';
+import { initializeMap } from './js/map.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img className="App-logo" alt="logo" />
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
