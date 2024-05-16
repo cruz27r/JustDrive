92ad544e0634a132bd644e96e26546368dae6923
Sergio Barro <115595685+Sylco123@users.noreply.github.com>
2024-05-16 16:03:54 -0400

    Update App.js

--- JustDrive-Web/src/App.js
+++ JustDrive-Web/src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import './App.css';
+import './App.css'; // Ensure you are importing the CSS file with the .logo class
 import { initializeMap } from './Map/map.js';
 import SearchComponent from './Components/SearchComponent';
 import TripComfortComponent from './Components/TripComfortComponent';
@@ -8,7 +8,6 @@ function App() {
     const mapRef = useRef(null);
     const directionsRef = useRef(null); // Reference for the directions container
     const [map, setMap] = useState(null);
-
     const [showTripComfortDetails, setShowTripComfortDetails] = useState(false);
 
     useEffect(() => {
@@ -21,23 +20,17 @@ function App() {
 
     return (
         <div className="App" style={{ position: 'relative', width: '100%', height: '100vh' }}>
-            {/* Add the logo at the top right */}
-            <div style={{ position: 'absolute', bottom: '10px', right: '10px' }}>
-                <img src={`${process.env.PUBLIC_URL}/images/JustDrive.png`} alt="App Logo" style={{ width: '150px', height: 'auto' }} />
+            {/* Add the logo with the CSS class */}
+            <img src={`${process.env.PUBLIC_URL}/images/JustDrive.png`} alt="App Logo" className="logo" />
+
+            <div style={{ display: 'flex', height: '100%' }}>
+                <div ref={mapRef} className='map-container' style={{ flex: 1 }}></div>
+                <TripComfortComponent map={map} showTripComfortDetails={showTripComfortDetails}/>  
             </div>
-                
-            <div style={{display: 'flex'}}>
-                <div ref={mapRef} className='map-container'></div>
-                {map && (
-                    <TripComfortComponent 
-                        map={map} 
-                        showTripComfortDetails={showTripComfortDetails} 
-                    />
-                )}            </div>
-            <div ref={directionsRef} id="directions-panel" >
+            <div ref={directionsRef} id="directions-panel">
                 {/* Directions will appear here */}
             </div>
-                <SearchComponent map={map} setShowTripComfortDetails={setShowTripComfortDetails} />
+            <SearchComponent map={map} setShowTripComfortDetails={setShowTripComfortDetails} />
         </div>
     );
 }
